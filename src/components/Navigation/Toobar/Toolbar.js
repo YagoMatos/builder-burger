1271ba95a56c88dfa87d288c25a33aae143e79f1
yagomatos <yago.matos@hotmail.com>
2019-01-16 22:51:51 -0200

    Criação do Navigation

---
+++ src/components/Navigation/Toobar/Toolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './Toobar.css';
+import Logo from '../../Logo/Logo';
+
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const toolbar = (props) => (
+    <header className={classes.Toobar}>
+        <div>MENU</div>
+        <Logo />
+        <nav>
+            <NavigationItems />
+        </nav>
+    </header>
+);
+
+export default toolbar;
