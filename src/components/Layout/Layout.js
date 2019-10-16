33bd486980498340e75968cbdcdbbd55edd94dbc
Yago Matos <yago.matos@gentrop.com>
2019-10-16 12:44:28 -0300

    finalização dos ajustes na responsividade

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
-import Toolbar from '../Navigation/Toobar/Toolbar';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 import classes from './NavigationItem.css';
 
-const navigationItem = (props) => (
+const navigationItem = ( props ) => (
     <li className={classes.NavigationItem}>
-        <a  
-            className={props.active ? classes.active : null} 
-            href={props.link}>{props.children}
-        </a>
+        <a 
+            href={props.link} 
+            className={props.active ? classes.active : null}>{props.children}</a>
     </li>
 );
 
