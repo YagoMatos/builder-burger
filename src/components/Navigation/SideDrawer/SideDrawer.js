b9c9f6efb1e98f462cd220f8b65d551d6128be9e
Yago Matos <yago.matos@gentrop.com>
2019-10-16 11:51:36 -0300

    ajustes na responsividade

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer';
+
+const sideDrawer = (props) => {
+  return(
+    <Aux>
+      <Backdrop show />
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+
+  )
+}
+
+export default sideDrawer;
