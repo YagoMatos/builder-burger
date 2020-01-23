47ecefce2d649820da082089cf62d937843c5242
Yago Matos <yago.matos@gentrop.com>
2020-01-23 16:36:16 -0300

    Finalização da logica de Autenticação com Firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,7 +9,8 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component{
   componentDidMount() {
-    this.props.onFetchOrders()
+    console.log(this.props.token)
+    this.props.onFetchOrders(this.props.token, this.props.userId);
   }
 
   render(){
@@ -33,12 +34,14 @@ class Orders extends Component{
 const mapStateToProps = state => {
   return {
     orders: state.order.orders,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token,
+    userId: state.auth.userId
   }
 }
 const mapDispatchToProps = dispatch => {
   return {
-    onFetchOrders: () => dispatch(actions.fetchOrders())
+    onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
   }
 }
 
